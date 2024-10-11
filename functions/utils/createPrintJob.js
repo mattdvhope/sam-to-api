cda107d6f3aba11bdbe8b5c46cef9cf16122d111
Matt Malone <mattagape@me.com>
2024-10-11 12:25:48 -0500

    updates

--- functions/utils/createPrintJob.js
+++ functions/utils/createPrintJob.js
@@ -24,34 +24,36 @@ const createPrintJob = async (orderSummary) => {
     const requestBody = {
         "contact_email": orderSummary.customer.email, // Email from orderSummary
         "external_id": `PbOrder-${timestampForOrder()}-${orderSummary.id}`, // Dynamic external_id with order ID
-        "line_items": orderSummary.items.map(item => ({
+        "line_items": [
+            {
             "external_id": `item-${item.product_id}`, // Unique reference for each item
             "printable_normalization": {
                 "cover": {
-                    // "source_url": "https://www.dropbox.com/s/7bv6mg2tj0h3l0r/lulu_trade_perfect_template.pdf?dl=1&raw=1"
                     "source_url": process.env.SAM_PB_CV
+                    // "source_url": "https://www.dropbox.com/s/7bv6mg2tj0h3l0r/lulu_trade_perfect_template.pdf?dl=1&raw=1"
                 },
                 "interior": {
-                    // "source_url": "https://www.dropbox.com/s/r20orb8umqjzav9/lulu_trade_interior_template-32.pdf?dl=1&raw=1"
                     "source_url": process.env.SAM_PB_IN
+                    // "source_url": "https://www.dropbox.com/s/r20orb8umqjzav9/lulu_trade_interior_template-32.pdf?dl=1&raw=1"
                 },
-                // "pod_package_id": "0600X0900BWSTDPB060UW444MXX"
                 "pod_package_id": process.env.SAM_PB_PPI
+                // "pod_package_id": "0600X0900BWSTDPB060UW444MXX" 
             },
             "quantity": item.quantity, // Quantity from orderSummary items
             "title": item.product_name // Product title from orderSummary
-        })),
-        "production_delay": 120,
-        "shipping_address": {
-            "city": orderSummary.shipping_address.city,
-            "country_code": orderSummary.shipping_address.country === 'USA' ? 'US' : orderSummary.shipping_address.country,
-            "name": `${orderSummary.customer.first_name} ${orderSummary.customer.last_name}`, // Full name from customer
-            "phone_number": orderSummary.customer.phone,
-            "postcode": orderSummary.shipping_address.postal_code,
-            "state_code": orderSummary.shipping_address.region, // State/region from orderSummary
-            "street1": orderSummary.shipping_address.address_line_1
-        },
-        "shipping_level": "MAIL" // Default or dynamic shipping level can be used here
+            }
+        ],
+      "production_delay": 120,
+      "shipping_address": {
+          "city": orderSummary.shipping_address.city,
+          "country_code": orderSummary.shipping_address.country === 'USA' ? 'US' : orderSummary.shipping_address.country,
+          "name": `${orderSummary.customer.first_name} ${orderSummary.customer.last_name}`, // Full name from customer
+          "phone_number": orderSummary.customer.phone,
+          "postcode": orderSummary.shipping_address.postal_code,
+          "state_code": orderSummary.shipping_address.region, // State/region from orderSummary
+          "street1": orderSummary.shipping_address.address_line_1
+      },
+      "shipping_level": "MAIL" // Default or dynamic shipping level can be used here
     };
 
     try {
