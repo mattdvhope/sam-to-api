19d3a3352c3fe484a3873202624810d3285f775e
Matt Malone <mattagape@me.com>
2024-10-12 17:38:17 -0500

    updates

--- functions/utils/createHcPrintJob.js
+++ functions/utils/createHcPrintJob.js
@@ -1,44 +1,17 @@
 // functions/utils/createHcPrintJob.js
-import axios from 'axios';
-import { getApiConfig } from './apiConfig';
-import showErrors from './showErrors';
-import validateOrderSummary from './validateOrderSummary';
-import createRequestBody from './createRequestBody'; // Import the request body function
-import createLineItems from './createLineItems'; // Import the new function.
+import createPrintJob from './createPrintJob'; // Import the new function
 
 const createHcPrintJob = async (orderSummary) => {
-    if (!validateOrderSummary(orderSummary)) return;
-
-    const { apiBaseURL, myHeaders } = await getApiConfig();
-
     const books = [];
-
     books.push({
         cover: process.env.SAM_HC_CV,
         interior: process.env.SAM_HC_IN,
         podPackageId: process.env.SAM_HC_PPI,
     });
 
-    // books.push({
-    //     cover: process.env.SAM_PB_CV,
-    //     interior: process.env.SAM_PB_IN,
-    //     podPackageId: process.env.SAM_PB_PPI,
-    // });
-
-    const lineItems = createLineItems(orderSummary, books);
     const shippingLevel = "MAIL";
-    const requestBody = createRequestBody(orderSummary, lineItems, shippingLevel);
 
-    try {
-        const response = await axios.post(
-            `${apiBaseURL}print-jobs/`,
-            requestBody,
-            { headers: myHeaders }
-        );
-        console.log(response.data.id);
-    } catch (errorData) {
-        console.error('Error Creating Print Job:', showErrors(errorData));
-    }
+    return createPrintJob(orderSummary, books, shippingLevel);
 };
 
 export default createHcPrintJob;
---
+++ functions/utils/createPrintJob.js
@@ -0,0 +1,29 @@
+// functions/utils/createPrintJob.js
+import axios from 'axios';
+import { getApiConfig } from './apiConfig';
+import showErrors from './showErrors';
+import validateOrderSummary from './validateOrderSummary';
+import createRequestBody from './createRequestBody';
+import createLineItems from './createLineItems';
+
+const createPrintJob = async (orderSummary, books, shippingLevel) => {
+    if (!validateOrderSummary(orderSummary)) return;
+
+    const { apiBaseURL, myHeaders } = await getApiConfig();
+
+    const lineItems = createLineItems(orderSummary, books);
+    const requestBody = createRequestBody(orderSummary, lineItems, shippingLevel);
+
+    try {
+        const response = await axios.post(
+            `${apiBaseURL}print-jobs/`,
+            requestBody,
+            { headers: myHeaders }
+        );
+        console.log(response.data.id);
+    } catch (errorData) {
+        console.error('Error Creating Print Job:', showErrors(errorData));
+    }
+};
+
+export default createPrintJob;
