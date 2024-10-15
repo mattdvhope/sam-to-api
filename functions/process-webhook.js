f1f899bcb5cab33afdd2e9df95b7c23dbf2e38ca
Matt Malone <mattagape@me.com>
2024-10-14 20:05:55 -0500

    updates

--- functions/process-webhook.js
+++ functions/process-webhook.js
@@ -1,35 +1,39 @@
 // functions/process-webhook.js
 import buildResponse from './utils/buildResponse';
 
+const statusActions = {
+    PRODUCTION_READY: {
+        subject: "Printing of book begins soon!!",
+        body: "Your book is about to be printed.",
+    },
+    IN_PRODUCTION: {
+        subject: "Your book is being printed now!!",
+        body: "Production is underway.",
+    },
+    SHIPPED: {
+        subject: "Your book has been shipped!!",
+        body: "Shipping details are being processed.",
+    },
+};
+
 exports.handler = async (event) => {
     if (event.httpMethod !== 'POST') {
-        return buildResponse(405, { message: 'Method Not Allowed' }); // Handle non-POST requests
+        return buildResponse(405, { message: 'Method Not Allowed' });
     }
 
     try {
-        // Parse the incoming webhook data
         const webhookData = JSON.parse(event.body);
         console.log('Received webhook data:', webhookData);
 
-        // Process the webhook data as needed
-        // For example, handle different event types:
-        if (webhookData.topic === "PRINT_JOB_STATUS_CHANGED") {
-            // Handle the PRINT_JOB_STATUS_CHANGED event
-            console.log('Handling PRINT_JOB_STATUS_CHANGED event...');
+        const { status: { name }, contact_email } = webhookData.data;
+
+        if (statusActions[name]) {
+            const { subject, body } = statusActions[name];
+            console.log(`EMAIL to ${contact_email}...`);
+            console.log(`Subject: ${subject}`);
+            console.log(`Body: ${body}`);
         }
 
-        console.log("Customer Email: ", webhookData.data.contact_email);
-
-
-        if (webhookData.data.status.name === "IN_PRODUCTION") {
-            console.log("EMAIL!!! > Your book is being printed now!!")
-        }
-
-        if (webhookData.data.status.name === "SHIPPED") {
-            console.log("EMAIL!!! > Your book has been shipped!!")
-        }
-
-        // Return a success response
         return buildResponse(200, { message: 'Webhook processed successfully' });
     } catch (error) {
         console.error('Error processing webhook:', error.message);
