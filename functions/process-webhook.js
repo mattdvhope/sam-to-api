b7a558b5a7c58d41b63aa1fc20f99887327b8a9d
Matt Malone <mattagape@me.com>
2024-10-16 08:32:54 -0500

    updates

--- functions/process-webhook.js
+++ functions/process-webhook.js
@@ -62,6 +62,8 @@ const addSubscriberToAudience = async (email, shippingAddress) => {
         },
     };
 
+console.log(subscriberData);
+
     // try {
     //     // First, check if the subscriber already exists in the audience.
     //     const response = await axios.get(url, {
@@ -74,9 +76,6 @@ const addSubscriberToAudience = async (email, shippingAddress) => {
     // } catch (error) {
     //     console.error('Error checking subscriber:', error.message);
     // }
-
-
-
 
 };
 
