d094a0fd44fadc2cf05a9e83d6a49d9ea9e04591
Matt Malone <mattagape@me.com>
2025-01-01 07:37:29 -0600

    updates

--- functions/webhook-helpers/statusActions.js
+++ functions/webhook-helpers/statusActions.js
@@ -8,14 +8,14 @@ import {
 } from './statusContent.js'; // Adjust the path as necessary
 
 const statusActions = {
-    PRODUCTION_DELAYED: {
-        subject: "Your book is on the docket to be printed!",
-        body: productionDelayedHtml // Replace with the imported HTML
-    },
-    PRODUCTION_READY: {
-        subject: "Printing of book begins soon!!",
-        body: productionReadyHtml // Replace with the imported HTML
-    },
+    // PRODUCTION_DELAYED: {
+    //     subject: "Your book is on the docket to be printed!",
+    //     body: productionDelayedHtml // Replace with the imported HTML
+    // },
+    // PRODUCTION_READY: {
+    //     subject: "Printing of book begins soon!!",
+    //     body: productionReadyHtml // Replace with the imported HTML
+    // },
     IN_PRODUCTION: {
         subject: "Your book is being printed now!!",
         body: inProductionHtml // Replace with the imported HTML
